19d92e15b8f5b8dd87d2a0af82306b1064eab14a
Rotem Liss <rotem@users.mediawiki.org>
2006-09-19 16:12:56 +0000

    Hebrew localisation for AntiSpoof.

--- AntiSpoof_i18n.php
+++ AntiSpoof_i18n.php
@@ -3,7 +3,11 @@
 $wgAntiSpoofMessages = array();
 $wgAntiSpoofMessages['en'] = array(
 	'antispoof-name-conflict' => 'The name "$1" is too similar to the existing account "$2". Please choose another name.',
-	'antispoof-name-illegal' => 'The name "$1" is not allowed to prevent confusing or spoofed usernames: $2. Please choose another name.',
+	'antispoof-name-illegal'  => 'The name "$1" is not allowed to prevent confusing or spoofed usernames: $2. Please choose another name.',
+);
+$wgAntiSpoofMessages['he'] = array(
+	'antispoof-name-conflict' => 'שם המשתמש "$1" שבחרתם דומה מדי לשם המשתמש הקיים "$2". אנא בחרו שם משתמש אחר.',
+	'antispoof-name-illegal'  => 'לא ניתן לבחור את שם המשתמש "$1" כדי למנוע שמות משתמש מבלבלים: $2. אנא בחרו שם משתמש אחר.',
 );
 
 ?>
