9a4ee2421c6f488147e337879241123ff60c212a
Brion Vibber <brion@users.mediawiki.org>
2006-09-19 14:24:42 +0000

    First draft of extension to block mixed-script/confusing and very similar usernames. Needs better logging, override options, i18n.

---
+++ AntiSpoof_i18n.php
@@ -0,0 +1,9 @@
+<?php
+
+$wgAntiSpoofMessages = array();
+$wgAntiSpoofMessages['en'] = array(
+	'antispoof-name-conflict' => 'The name "$1" is too similar to the existing account "$2". Please choose another name.',
+	'antispoof-name-illegal' => 'The name "$1" is not allowed to prevent confusing or spoofed usernames: $2. Please choose another name.',
+);
+
+?>
