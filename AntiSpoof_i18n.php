768c09ca7078ca44053569c5780e1f6627aa163d
Jimmy Collins <collinj@users.mediawiki.org>
2006-09-20 20:49:31 +0000

    (bug 7376) Czech translation for AntiSpoof, Makesysop, Renameuser, Newuserlog

--- AntiSpoof_i18n.php
+++ AntiSpoof_i18n.php
@@ -5,6 +5,10 @@ $wgAntiSpoofMessages['en'] = array(
 	'antispoof-name-conflict' => 'The name "$1" is too similar to the existing account "$2". Please choose another name.',
 	'antispoof-name-illegal'  => 'The name "$1" is not allowed to prevent confusing or spoofed usernames: $2. Please choose another name.',
 );
+$wgAntiSpoofMessages['cs'] = array(
+	'antispoof-name-conflict' => 'Uživatelské jméno "$1" je příliš podobné existujícímu účtu "$2". Prosím, vyberte si jiné jméno.',
+	'antispoof-name-illegal'  => 'Uživatelské jméno "$1" není povoleno vytvořit, aby se nepletlo nebo nesloužilo k napodobování uživatelského jména: $2. Prosím, vyberte si jiné jméno.',
+);
 $wgAntiSpoofMessages['de'] = array(
 	'antispoof-name-conflict' => 'Der gewünschte Benutzername „$1“ ist dem bereits vorhandenen Benutzernamen „$2“ zu ähnlich. Bitte wählen Sie einen anderen Benutzernamen.',
 	'antispoof-name-illegal'  => 'Der gewünschte Benutzername „$1“ ist nicht erlaubt, um Verwirrungen und Täuschungen zu dem vorhandenen Benutzernamen „$2“ zu verhindern. Bitte wählen Sie einen anderen Benutzernamen.',
