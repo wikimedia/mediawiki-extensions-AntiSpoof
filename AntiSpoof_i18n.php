11719429bb3ed98074067cedf6f243e28fe4dee6
Jimmy Collins <collinj@users.mediawiki.org>
2006-09-20 16:54:02 +0000

    Added German translation (de)

--- AntiSpoof_i18n.php
+++ AntiSpoof_i18n.php
@@ -5,6 +5,10 @@ $wgAntiSpoofMessages['en'] = array(
 	'antispoof-name-conflict' => 'The name "$1" is too similar to the existing account "$2". Please choose another name.',
 	'antispoof-name-illegal'  => 'The name "$1" is not allowed to prevent confusing or spoofed usernames: $2. Please choose another name.',
 );
+$wgAntiSpoofMessages['de'] = array(
+	'antispoof-name-conflict' => 'Der gewünschte Benutzername „$1“ ist dem bereits vorhandenen Benutzernamen „$2“ zu ähnlich. Bitte wählen Sie einen anderen Benutzernamen.',
+	'antispoof-name-illegal'  => 'Der gewünschte Benutzername „$1“ ist nicht erlaubt, um Verwirrungen und Täuschungen zu dem vorhandenen Benutzernamen „$2“ zu verhindern. Bitte wählen Sie einen anderen Benutzernamen.',
+);
 $wgAntiSpoofMessages['he'] = array(
 	'antispoof-name-conflict' => 'שם המשתמש "$1" שבחרתם דומה מדי לשם המשתמש הקיים "$2". אנא בחרו שם משתמש אחר.',
 	'antispoof-name-illegal'  => 'לא ניתן לבחור את שם המשתמש "$1" כדי למנוע שמות משתמש מבלבלים: $2. אנא בחרו שם משתמש אחר.',
