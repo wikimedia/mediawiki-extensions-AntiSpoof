5784878915535dfbc59c4891264756dd75768ac6
Raimond Spekking <raymond@users.mediawiki.org>
2008-02-01 16:54:26 +0000

    Localisation updates German

--- AntiSpoof.i18n.php
+++ AntiSpoof.i18n.php
@@ -173,6 +173,7 @@ $messages['da'] = array(
 );
 
 $messages['de'] = array(
+	'antispoof-desc'          => 'Verhindert die Erstellung von Benutzerkonten mit gemischten Zeichensätzen, verwirrenden und ähnlichen Benutzernamen',
 	'antispoof-name-conflict' => 'Der gewünschte Benutzername „$1“ ist dem bereits vorhandenen Benutzernamen „$2“ zu ähnlich. Bitte einen anderen Benutzernamen wählen.',
 	'antispoof-name-illegal'  => 'Der gewünschte Benutzername „$1“ ist nicht erlaubt. Grund: $2<br />Bitte einen anderen Benutzernamen wählen.',
 	'antispoof-badtype'       => 'Ungültiger Datentyp',
