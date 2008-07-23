c1caecdde17a285d432345df8adb0ca0091b0322
Brion Vibber <brion@users.mediawiki.org>
2008-07-23 08:15:47 +0000

    Revert r37932, r37933 -- this is just downright bizarre. o_O
    Format the list as a *list*. There's no need for multiple messages with different numbers of parameters!
    
    Also use the {{PLURAL:}} parser function where necessary, rather than creating separate messages for "account" and "accounts" etc.

--- AntiSpoof.i18n.php
+++ AntiSpoof.i18n.php
@@ -9,13 +9,8 @@ $messages = array();
 
 $messages['en'] = array(
 	'antispoof-desc'          => 'Blocks the creation of accounts with mixed-script, confusing and similar usernames',
-	'antispoof-name-conflict' => 'The name "$1" is too similar to existing $2.
+	'antispoof-name-conflict' => 'The name "$1" is too similar to the existing account "$2".
 Please choose another name.',
-	'antispoof-name-conflict1' => 'account: "$1"',
-	'antispoof-name-conflict2' => 'accounts: "$1", "$2"',
-	'antispoof-name-conflict3' => 'accounts: "$1", "$2", "$3"',
-	'antispoof-name-conflict4' => 'accounts: "$1", "$2", "$3", "$4"',
-	'antispoof-name-conflict5' => 'accounts: "$1", "$2", "$3", "$4", "$5"',
 	'antispoof-name-illegal'  => 'The name "$1" is not allowed to prevent confusing or spoofed usernames: $2.
 Please choose another name.',
 	'antispoof-badtype'       => 'Bad data type',
