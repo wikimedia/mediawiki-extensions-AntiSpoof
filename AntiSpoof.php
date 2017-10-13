87c90e1eb400f65997f93ccbdf2bb778cafd81f8
James D. Forrester <jforrester@wikimedia.org>
2017-10-13 14:21:26 -0700

    Uncomment wfWarn in deprecated PHP entry point
    Change-Id: Ifc455833721fc1ad9592f2356d33783f828294fa

--- AntiSpoof.php
+++ AntiSpoof.php
@@ -3,11 +3,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'AntiSpoof' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['AntiSpoof'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for AntiSpoof extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
 	die( 'This version of the AntiSpoof extension requires MediaWiki 1.25+' );
